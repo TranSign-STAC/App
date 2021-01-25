0dfe44f3f4241dee4027e3758bb42a93c7cb409d
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-01-26 03:56:50 +0900

    Add lib/src/views/screens/home/widgets/translating_dialog.dart

---
+++ lib/src/views/screens/home/widgets/translating_dialog.dart
@@ -0,0 +1,58 @@
+import 'package:app/src/configs/theme.dart';
+import 'package:app/src/views/widgets/dialog.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class TranslatingDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Widget content = Container(
+      child: Container(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 40),
+            ),
+            Container(
+              child: Container(
+                width: 128,
+                height: 128,
+                decoration: BoxDecoration(
+                  color: TransignColors.PrimaryColor,
+                  shape: BoxShape.circle,
+                ),
+                child: Padding(
+                  child:
+                      SvgPicture.asset("assets/images/crossed_arrows_big.svg"),
+                  padding: EdgeInsets.all(40),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 24),
+            ),
+            Container(
+              child: Text(
+                "번역중..",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w700,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: 16),
+            ),
+          ],
+        ),
+      ),
+      width: MediaQuery.of(context).size.width,
+    );
+    return TransignDialog(
+      "취소",
+      content: content,
+      actionCallback: () => {Navigator.of(context).pop()},
+    );
+  }
+}
