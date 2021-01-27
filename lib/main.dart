cba99b632cea45b2205d27b2218afda58b7d8915
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-01-27 19:35:48 +0900

    Update main.dart to disable rotation

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return MaterialApp(
       themeMode: ThemeMode.dark,
       home: HomeScreen(),
