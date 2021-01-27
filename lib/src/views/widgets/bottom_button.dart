fe4eec586f94f4ef62313e22ea71d8bc536e562e
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-01-27 20:23:23 +0900

    Add ripple effect to bottom button in lib/src/views/widgets/bottom_button.dart

--- lib/src/views/widgets/bottom_button.dart
+++ lib/src/views/widgets/bottom_button.dart
@@ -18,30 +18,45 @@ class BottomButton extends StatelessWidget {
     Color fontColor =
         this.enabled ? Colors.white : TransignColors.BlackScale[400];
     FontWeight fontWeight = this.enabled ? FontWeight.w700 : FontWeight.w400;
-    return InkWell(
-      onTap: this.enabled ? onPressed : null,
-      child: Container(
-        child: Container(
-          child: Text(
-            this.text,
-            style: TextStyle(
-              color: fontColor,
-              fontSize: 16,
-              fontWeight: fontWeight,
-            ),
-            textAlign: TextAlign.center,
+    Container(
+      child: new Material(
+        child: new InkWell(
+          onTap: () {
+            print("tapped");
+          },
+          child: new Container(
+            width: 100.0,
+            height: 100.0,
           ),
-          width: 375,
-          height: 56,
-          padding: const EdgeInsets.symmetric(
-            vertical: 16,
-          ),
-          decoration: BoxDecoration(color: buttonColor),
         ),
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).padding.bottom +
-            56, // navigation bar + button
+        color: Colors.transparent,
       ),
+      color: Colors.orange,
+    );
+
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height:
+          MediaQuery.of(context).padding.bottom + 56, // navigation bar + button
+      child: Material(
+        child: InkWell(
+          child: Padding(
+            padding: EdgeInsets.only(top: 16),
+            child: Text(
+              this.text,
+              style: TextStyle(
+                color: fontColor,
+                fontSize: 16,
+                fontWeight: fontWeight,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          onTap: this.enabled ? onPressed : null,
+        ),
+        color: Colors.transparent,
+      ),
+      color: buttonColor,
     );
   }
 }
