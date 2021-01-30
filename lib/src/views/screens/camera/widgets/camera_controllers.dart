28e0f898bea6f11f45d21c0d1a7d1c3a40d6a305
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-01-31 03:48:51 +0900

    Fix wrong import in CameraControllers and SpeedController

--- lib/src/views/screens/camera/widgets/camera_controllers.dart
+++ lib/src/views/screens/camera/widgets/camera_controllers.dart
@@ -1,6 +1,6 @@
 import 'package:app/src/configs/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/svg.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class CameraControllers extends StatefulWidget {
   CameraControllers({
