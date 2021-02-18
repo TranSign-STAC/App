ebff55126efba89844c652933b1d2ff500acd055
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-02-19 02:35:13 +0900

    Refactor lib/src/views/screens/translation_result/widgets/video_player.dart to have a parameter
    This commit adds a parameter to SignLangaugeVideoPlayer:
    - String renderUrl

--- lib/src/views/screens/treanslation_result/widgets/video_player.dart
+++ lib/src/views/screens/treanslation_result/widgets/video_player.dart
@@ -2,14 +2,16 @@ import 'package:app/src/configs/theme.dart';
 import 'package:flutter/material.dart';
 
 class SignLanguageVideoPlayer extends StatelessWidget {
+  const SignLanguageVideoPlayer(this.renderUrl);
+  final String renderUrl;
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 213,
-      decoration: BoxDecoration(
-        color: TransignColors.BlackScale[300],
-      ),
-    );
+        width: MediaQuery.of(context).size.width,
+        height: 213,
+        decoration: BoxDecoration(
+          color: TransignColors.BlackScale[300],
+        ),
+        child: Text('${this.renderUrl}'));
   }
 }
