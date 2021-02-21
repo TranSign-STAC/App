406a356865b7fe701906732ec6bd24d37eab4fed
YeonGyu Kim <code.yeon.gyu@gmail.com>
2021-02-21 21:05:03 +0900

    Update lib/src/views/screens/history/history.dart to add REST API communication feature

--- lib/src/views/screens/history/history.dart
+++ lib/src/views/screens/history/history.dart
@@ -1,24 +1,25 @@
 import 'package:app/src/configs/theme.dart';
+import 'package:app/src/models/api.dart';
+import 'package:app/src/models/favorite_translation.dart';
 import 'package:app/src/models/translation_history.dart';
 import 'package:app/src/views/widgets/appbar.dart';
 import 'package:app/src/views/widgets/bottom_button.dart';
 import 'package:app/src/views/widgets/translation_box.dart';
+import 'package:dio/dio.dart';
 import "package:flutter/material.dart";
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HistoryScreen extends StatefulWidget {
   _HistoryScreen createState() => _HistoryScreen();
 }
 
 class _HistoryScreen extends State<HistoryScreen> {
+  final client = ApiClient(Dio(BaseOptions()));
+  String uuid;
   bool editingEnabled = false;
-
-  List<TranslationHistory> historyData = [
-    TranslationHistory("Screwed up"),
-    TranslationHistory(
-        "TranSign이란, 한국 수어 번역 및 웹 페이지 수어 번역 솔루션입니다. TranSign이란, 한국 수어 번역 및 웹 페이지 수어 번역 솔루션입니다. TranSign이란, 한국 수어 번역 및 웹 페이지 수어 번역 솔루션입니다. TranSign이란, 한국 수어 번역 및 웹 페이지 수어 번역 솔루션입니다. TranSign이란, 한국 수어 번역 및 웹 페이지 수어 번역...")
-  ];
+  List<TranslationHistory> historyData = [];
 
   Widget _translationBox(String text, bool marked, VoidCallback onTap) {
     String iconPath = this.editingEnabled
@@ -44,50 +45,7 @@ class _HistoryScreen extends State<HistoryScreen> {
         color: Colors.transparent);
     return TranslationBox(text, button: button);
   }
-
-  List<Widget> _renderHistoryTranslationBoxes() {
-    final List<Widget> favoriteTranslationBoxes =
-        new List(this.historyData.length);
-    historyData.asMap().forEach(
-      (i, data) {
-        favoriteTranslationBoxes[i] = this._translationBox(
-          data.text,
-          data.favorited,
-          () {
-            this.setState(
-              () {
-                historyData[i].favorited = !historyData[i].favorited;
-              },
-            );
-          },
-        );
-      },
-    );
-    return favoriteTranslationBoxes;
-  }
-
-  List<Widget> _renderFavoriteTranslationBoxes() {
-    final List<Widget> historyTranslationBoxes =
-        new List(this.historyData.length);
-
-    this.historyData.asMap().forEach(
-      (i, data) {
-        historyTranslationBoxes[i] = this._translationBox(
-          data.text,
-          data.marked,
-          () {
-            this.setState(
-              () {
-                this.historyData[i].marked = !this.historyData[i].marked;
-              },
-            );
-          },
-        );
-      },
-    );
-
-    return historyTranslationBoxes;
-  }
+  // widget
 
   void enableEditing() {
     this.setState(() {
@@ -114,6 +72,7 @@ class _HistoryScreen extends State<HistoryScreen> {
       ),
     );
   }
+  // actionbar actions
 
   int countSelectedBoxExists() {
     int selectedBoxCount = 0;
@@ -132,6 +91,96 @@ class _HistoryScreen extends State<HistoryScreen> {
       }
     }
     return false;
+  }
+
+  Future<Map<String, dynamic>> getHistory(uuid) async {
+    Map<String, dynamic> rawData = {
+      "history": await this.client.getHistory(uuid),
+      "favorite": await this.client.getFavorite(uuid),
+    };
+    return rawData;
+  }
+
+  void loadTranslationHistory() async {
+    final SharedPreferences pref = await SharedPreferences.getInstance();
+    this.uuid = pref.getString("uuid");
+    final queriedData = await this.getHistory(this.uuid);
+    final List<TransignTranslationHistory> rawHistory = queriedData["history"]
+        .history; // set reversed order because server returns oldest order
+    final List<String> favorites = queriedData['favorite'].favorites;
+    final List<String> histories = [];
+    rawHistory.asMap().forEach((int i, TransignTranslationHistory data) {
+      if (!histories.contains(data.text)) {
+        histories.add(data.text);
+      }
+    }); // remove duplicates
+    this.historyData = new List(histories.length);
+    this.setState(() {
+      histories.asMap().forEach((int i, String text) {
+        this.historyData[i] =
+            TranslationHistory(text, favorited: favorites.contains(text));
+      }); // convert to TranslationHistory
+    });
+  }
+  // utils
+
+  List<Widget> _renderHistoryTranslationBoxes() {
+    final List<Widget> historyTranslationBoxes =
+        new List(this.historyData.length);
+
+    this.historyData.asMap().forEach(
+      (int i, TranslationHistory data) {
+        historyTranslationBoxes[i] = this._translationBox(
+          data.text,
+          data.favorited,
+          () {
+            this.setState(
+              () {
+                this.historyData[i].favorited = !this.historyData[i].favorited;
+              },
+            );
+            this.client.toggleFavorite(
+                  TransignToggleFavoriteTranslationRequest(
+                    text: data.text,
+                    uuid: this.uuid,
+                  ),
+                );
+          },
+        );
+      },
+    );
+
+    return historyTranslationBoxes;
+  }
+
+  List<Widget> _renderFavoriteTranslationBoxes() {
+    final List<Widget> historyTranslationBoxes =
+        new List(this.historyData.length);
+
+    this.historyData.asMap().forEach(
+      (i, data) {
+        historyTranslationBoxes[i] = this._translationBox(
+          data.text,
+          data.marked,
+          () {
+            this.setState(
+              () {
+                this.historyData[i].marked = !this.historyData[i].marked;
+              },
+            );
+          },
+        );
+      },
+    );
+
+    return historyTranslationBoxes;
+  }
+  // renderers
+
+  @override
+  void initState() {
+    super.initState();
+    this.loadTranslationHistory();
   }
 
   @override
@@ -159,62 +208,63 @@ class _HistoryScreen extends State<HistoryScreen> {
         )
       ],*/
     );
-
-    TransignAppBar editingAppBar = TransignAppBar(
-      centerTitle: false,
-      enableDarkMode: true,
-      color: TransignColors.SubRed,
-      leading: IconButton(
-        icon: SvgPicture.asset("assets/images/crossed_white.svg"),
-        onPressed: this.disableEditing,
-      ),
-      title: Text("N개 선택",
-          style: TextStyle(
-              color: Colors.white, fontWeight: FontWeight.w700, fontSize: 16)),
-      actions: [
-        Padding(
-          padding: EdgeInsets.only(right: 16),
-          child: Center(
-            child: GestureDetector(
-              child: Text(
-                "전체 선택",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 13,
-                  fontWeight: FontWeight.w300,
-                ),
-                textAlign: TextAlign.right,
-              ),
-              onTap: this.selectAllBoxes,
-            ),
-          ),
-        )
-      ],
-    );
-
     Scaffold normalScaffold = Scaffold(
       appBar: normalAppBar,
-      body: Column(children: this._renderHistoryTranslationBoxes()),
-    );
-
-    Scaffold editingScaffold = Scaffold(
-      appBar: editingAppBar,
-      body: Column(
-        children: this._renderFavoriteTranslationBoxes() +
-            [
-              Expanded(
-                  child: Align(
-                      alignment: Alignment.bottomCenter,
-                      child: BottomButton(
-                        "삭제하기",
-                        onPressed: () {},
-                        enabled: this.isSelectedBoxExists(),
-                        enabledColor: TransignColors.SubRed,
-                      ))),
-            ],
-      ),
+      body: this.historyData.length == 0
+          ? Center(
+              child: SizedBox(
+                  child: CircularProgressIndicator(), width: 60, height: 60))
+          : ListView(children: this._renderHistoryTranslationBoxes()),
     );
     return normalScaffold;
     //return this.editingEnabled ? editingScaffold : normalScaffold;
+    // TransignAppBar editingAppBar = TransignAppBar(
+    //   centerTitle: false,
+    //   enableDarkMode: true,
+    //   color: TransignColors.SubRed,
+    //   leading: IconButton(
+    //     icon: SvgPicture.asset("assets/images/crossed_white.svg"),
+    //     onPressed: this.disableEditing,
+    //   ),
+    //   title: Text("N개 선택",
+    //       style: TextStyle(
+    //           color: Colors.white, fontWeight: FontWeight.w700, fontSize: 16)),
+    //   actions: [
+    //     Padding(
+    //       padding: EdgeInsets.only(right: 16),
+    //       child: Center(
+    //         child: GestureDetector(
+    //           child: Text(
+    //             "전체 선택",
+    //             style: TextStyle(
+    //               color: Colors.white,
+    //               fontSize: 13,
+    //               fontWeight: FontWeight.w300,
+    //             ),
+    //             textAlign: TextAlign.right,
+    //           ),
+    //           onTap: this.selectAllBoxes,
+    //         ),
+    //       ),
+    //     )
+    //   ],
+    // );
+    // Scaffold editingScaffold = Scaffold(
+    //   appBar: editingAppBar,
+    //   body: Column(
+    //     children: this._renderFavoriteTranslationBoxes() +
+    //         [
+    //           Expanded(
+    //               child: Align(
+    //                   alignment: Alignment.bottomCenter,
+    //                   child: BottomButton(
+    //                     "삭제하기",
+    //                     onPressed: () {},
+    //                     enabled: this.isSelectedBoxExists(),
+    //                     enabledColor: TransignColors.SubRed,
+    //                   ))),
+    //         ],
+    //   ),
+    // );
   }
 }
